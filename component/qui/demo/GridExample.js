9b2ba19afb64d457998538a300d326069010ac94
fayching <435462399@163.com>
2016-06-13 20:52:00 +0800

    d

--- component/qui/demo/GridExample.js
+++ component/qui/demo/GridExample.js
@@ -12,34 +12,49 @@ import React, {
 import Subheader from '../view/Subheader';
 import Grid from '../view/Grid';
 import Col from '../view/Col';
+import GridView from '../view/GridView';
 
 export default class GridExample extends Component{
 
     render() {
     	return(
+            <View>
+            <Grid style={{paddingTop:40,width: 200}}>
+                <Col span={50}>
+                    <Text>box-1</Text>
+                </Col>
+                <Col span={50}>
+                    <Text>box-2</Text>
+                </Col>
+                <Col span={33.333}>
+                    <Text>box-1</Text>
+                </Col>
+                <Col span={33.333}>
+                    <Text>box-2</Text>
+                </Col>
+                <Col span={33.333}>
+                    <Text>box-1</Text>
+                </Col>
+                <Col span={33.333}>
+                    <Text>box-2</Text>
+                </Col>
 
-        <Grid style={{paddingTop:40,width: 200}}>
-            <Col span={50}>
-                <Text>box-1</Text>
-            </Col>
-            <Col span={50}>
-                <Text>box-2</Text>
-            </Col>
-            <Col span={33.333}>
-                <Text>box-1</Text>
-            </Col>
-            <Col span={33.333}>
-                <Text>box-2</Text>
-            </Col>
-            <Col span={33.333}>
-                <Text>box-1</Text>
-            </Col>
-            <Col span={33.333}>
-                <Text>box-2</Text>
-            </Col>
-
-        </Grid>
-
+            </Grid>
+            <GridView columns=2>
+                <GridView.Item span={33.333}
+                    primaryText="这是标题"
+                    subText="这是内容"
+                    imgUri="http://qvas.xyz/vipstyle/mobile/client/cartoon/v2/pic/tribe/tribe1.png"
+                    imgType="square">
+                </GridView.Item>
+                <GridView.Item span={33.333}
+                    primaryText="这是标题"
+                    subText="这是内容"
+                    imgUri="http://qvas.xyz/vipstyle/mobile/client/cartoon/v2/pic/tribe/tribe1.png"
+                    imgType="square">
+                </GridView.Item>
+            </GridView>
+        </View>
 		);
     }
 }
--- component/qui/view/GridView.js
+++ component/qui/view/GridView.js
@@ -1,28 +1,30 @@
 'use strict';
 import React, {
     Component,
+    PropTypes,
+} from 'react';
+import {
     View,
     Text,
+    Image,
     StyleSheet,
+    Dimensions,
 } from 'react-native';
 
 import styles from '../style/Grid.css';
 import Grid from './Grid';
 import Col from './Col';
 
-export default class GridView extends Component {
+export default class GridView extends React.Component {
     constructor(props) {
         super(props);
     };
     static defaultProps = {
         spacing: 0,
         width: Dimensions.get('window').width,
-        imgRatio: 1,
     };
     static propTypes = {
-        style: View.PropTypes.style,
         elementStyles: PropTypes.object,
-        elements: PropTypes.object,
     };
     state = {};
 	render = () => {
@@ -30,36 +32,78 @@ export default class GridView extends Component {
             style,
             elementStyles,
             width,
-            elements,
             columns,
             spacing,
-            imgType,
-            imgRatio,
         } = this.props;
-        let imgHeight;
-        if(imgUri){
-            let colWidth = (width + spacing) * span/100;
-            let ratio = imgRatio;
-            if(imgType&&imgType=='square'){
-                ratio = 1;
-            }else if (imgType&&imgType=='vertical') {
-                ratio = 7/10;
-            }else if (imgType&&imgType=='horizontal') {
-                ratio = 10/7;
-            }
-            imgHeight = colWidth * ratio;
-        }
+
+        let colWidth = columns?(width + spacing)/columns;
+
+		let _children = Array.isArray(this.props.children)?this.props.children:[this.props.children];
+        let children = _children.map((o, i)=>{
+            return React.cloneElement(o,{ style: {height: 100}, key: i });
+        });
+
         return (
-            <Grid style={[{marginRight: -spacing}],style}>
-            {elements.map((element) =>
-                <Col style={[styles.col,{ flex: 100/columns },spacing && {marginRight: spacing}, elementStyles && elementStyles.col]}>
-                    {element.imgUri && <Image style={[styles.img, {height: imgHeight}, elementStyles && elementStyles.img]} source = {{uri: element.imgUri}}/>}
-                    {element.title && <Text style={styles.title, elementStyles && elementStyles.title}>{element.title}</Text> }
-                    {element.subText &&  <Text style={styles.subText, elementStyles && elementStyles.subText}>{element.subText}</Text> }
-                    {element.children && {element.children}}
-                </Col>
-            )}
+            <Grid>
+                { this.props.children }
             </Grid>
         )
 	}
 }
+class Item extends React.Component{
+    constructor(props) {
+        super(props);
+    };
+    static defaultProps = {
+        imgRatio: 1,
+        primaryTextLines: 1,
+        subTextLines: 1,
+    };
+    static propTypes = {
+        elementStyles: PropTypes.object,
+        width: PropTypes.string,
+        imgUri: PropTypes.string,
+        primaryText: PropTypes.string,
+        primaryTextLines: PropTypes.number,
+        subText: PropTypes.string,
+        subTextLines: PropTypes.number,
+        columns: PropTypes.number,
+        spacing: PropTypes.string,
+        imgType: PropTypes.oneOf(['square','vertical','horizontal']),
+        imgRatio: PropTypes.number,
+    };
+    render = () => {
+        const {
+            style,
+            width,
+            imgUri,
+            primaryText,
+            primaryTextLines,
+            subText,
+            subTextLines,
+            spacing,
+            imgType,
+            imgRatio,
+            elementStyles,
+        } = this.props;
+        let imgHeight;
+        let ratio = imgRatio;
+        if(imgType&&imgType=='square'){
+            ratio = 1;
+        }else if (imgType&&imgType=='vertical') {
+            ratio = 7/10;
+        }else if (imgType&&imgType=='horizontal') {
+            ratio = 10/7;
+        }
+        imgHeight = width * ratio;
+        return (
+            <Col>
+                {imgUri && <Image style={[styles.img,{width: width}, {height: imgHeight}, elementStyles && elementStyles.img]} source = {{uri: imgUri}}/>}
+                {primaryText && <Text style={styles.primaryText, elementStyles && elementStyles.primaryText} numberOfLines={primaryTextLines}>{primaryText}</Text> }
+                {subText &&  <Text style={styles.subText, elementStyles && elementStyles.subText} numberOfLines={subTextLines}>{subText}</Text> }
+                {this.props.children}
+            </Col>
+        )
+    }
+}
+GridView.Item = Item;
